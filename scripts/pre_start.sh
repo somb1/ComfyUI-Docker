bf0e9b2c3d1c11df25f9d651a3c83d6385ef91a2
somb1 <somersbyappl@gmail.com>
2025-02-16 18:08:57 +0900

    edit sh

--- scripts/pre_start.sh
+++ scripts/pre_start.sh
@@ -2,10 +2,10 @@
 
 export PYTHONUNBUFFERED=1
 
-echo "**** [1/3] syncing venv to workspace, please wait. This could take a while on first startup! ****"
+echo "**** syncing venv to workspace, please wait. This could take a while on first startup! ****"
 rsync -au --remove-source-files /venv/ /workspace/venv/ && rm -rf /venv
 
-echo "**** [2/3] Updating VIRTUAL_ENV from '/venv' to '/workspace/venv' ****"
+echo "**** Updating VIRTUAL_ENV from '/venv' to '/workspace/venv' ****"
 find "/workspace/venv/bin" -type f | while read -r file; do
     if file "$file" | grep -q "text"; then
         sed -i 's|/venv|/workspace/venv|g' "$file"
@@ -13,7 +13,7 @@ find "/workspace/venv/bin" -type f | while read -r file; do
     fi
 done
 
-echo "**** [3/3] syncing ComfyUI to workspace, please wait ****"
+echo "**** syncing ComfyUI to workspace, please wait ****"
 rsync -au --remove-source-files /ComfyUI/ /workspace/ComfyUI/ && rm -rf /ComfyUI
 rsync -au --remove-source-files /comfy-models/ /workspace/ComfyUI/models/ && rm -rf /ComfyUI /comfy-models
 
