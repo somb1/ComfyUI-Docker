02de37c27b44d04bfadd6c8ece096a17c704da6d
somb1 <somersbyappl@gmail.com>
2025-02-16 22:19:21 +0900

    edit sh

--- scripts/pre_start.sh
+++ scripts/pre_start.sh
@@ -14,13 +14,14 @@ done
 
 echo "**** syncing ComfyUI to workspace, please wait ****"
 rsync -au --remove-source-files /ComfyUI/ /workspace/ComfyUI/ && rm -rf /ComfyUI
-ln -s /comfy-checkpoints/* /workspace/ComfyUI/models/checkpoints/
-ln -s /comfy-upscale_models/* /workspace/ComfyUI/models/upscale_models/
+ln -sf /comfy-checkpoints/* /workspace/ComfyUI/models/checkpoints/
+ln -sf /comfy-upscale_models/* /workspace/ComfyUI/models/upscale_models/
 
-#echo "**** Setting the timezone based on the TZ environment variable. If not set, it defaults to Etc/UTC. ****" && \
+#echo "**** Setting the timezone based on the TIME_ZONE environment variable. If not set, it defaults to Etc/UTC. ****" && \
 export TZ=${TIME_ZONE:-"Etc/UTC"} && \
 echo "**** Timezone set to $TZ ****" && \
-echo "$TZ" | sudo tee /etc/timezone && \
+echo "$TZ" | sudo tee /etc/timezone > /dev/null && \
+sudo ln -sf "/usr/share/zoneinfo/$TZ" /etc/localtime && \
 sudo dpkg-reconfigure -f noninteractive tzdata
 
 source /workspace/venv/bin/activate
